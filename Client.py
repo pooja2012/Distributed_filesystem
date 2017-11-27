513d70e7dfbee4f32623964240d9466b97733167
pooja2012 <33168116+pooja2012@users.noreply.github.com>
2017-11-27 20:53:50 +0000

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,8 +1,42 @@
-import json
-import requests as req
-url="http://0.0.0.0:8080/"
+import socket               # Import socket module
 
-filepath=raw_input("Enter the file name:")
-file=url+filepath
-response=req.get(url)
-print("Response",response.text)
+def directory():
+    
+    file_name = raw_input("Enter File Name, type exit to terminate: ")
+    socket_dir.send(file_name.encode())    
+    data = socket_dir.recv(2048)
+    data1 = data.decode()
+    print data1
+    
+    file_name1 = raw_input("Type open to access file and exit to quit or back to open another file ")
+    if file_name1 == 'open':
+        data2 = 'exit'
+        socket_dir.send(data2.encode())
+        fileserver(file_name)
+        
+    elif file_name1 == 'exit':
+        file_name1.encode()
+        socket_dir.send(file_name1.encode())
+        print'Application Terminated'
+    elif file_name1 == 'back':
+        file_name = raw_input("Enter File Name, type exit to terminate: ")
+        socket_dir.send(file_name.encode())
+        data = socket_dir.recv(2048)
+        data1 = data.decode()
+        print data1
+    socket_dir.close()
+
+
+
+
+if __name__ == '__main__':
+   
+    host = 'localhost'
+    port_dir = 5000
+    socket_dir = socket.socket()
+    socket_dir.connect((host, port_dir))
+    
+    
+    directory()
+    
+
