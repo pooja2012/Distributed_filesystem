7e21e0a4f3fe2e543ba711332b8a68c45019c8c7
pooja2012 <33168116+pooja2012@users.noreply.github.com>
2017-11-30 10:04:27 +0000

    Update fileServer.py

--- fileServer.py
+++ fileServer.py
@@ -1,38 +1,75 @@
+import os.path
 import socket
-import threading
-import os
+import select
+import sys
+from thread import *
 from threading import Thread
-## Retrieving file
-def RetrFile(name,sock):
-    filename=sock.recv(1024)
-    if os.path.isfile(filename):
-        sock.send("EXISTS"+str(os.path.getsize(b'filename')))
-        userResponse=sock.recv(1024)
-        if userResponse[:2] == 'OK':
-            with open(filename,'rb') as f:
-                bytesToSend= f.read(1024)
-                sock.send(bytesToSend)
-                while bytesToSend != "":
-                    bytesToSend=f.read(1024)
-                    sock.send(bytesToSend)
-    else:
-          sock.send("ERR")
-          sock.close()
-def Main():
-    host='127.0.0.1'
-    port=5000
-    s=socket.socket()
-    s.bind((host,port))
-    s.listen(5)
-    print("Server started")
-    while True:
-        c,addr = s.accept()
-        print("Client connected the ip:<"+str(addr) + ">")
-        print("------------------------")
-        t=threading.Thread(target=RetrFile,args=("retrThread",c))
-        t.start()
-        print("Got the thread")
 
-    s.close()
-if __name__ == '__main__':
+class client_thread(Thread):
+
+       def __init__(self,addr,c):
+              Thread.__init__(self)
+              self.addr = addr
+              self.c = c
+
+       def run(self):
+              while True:
+                     file_server(self.addr,self.c)
+              
+
+def file_server(name,c):
+
+       path = os.getcwd()
+       dir = os.listdir(path)
+       print 'Server started'
+
+
+       a = c.recv(1024)
+       remove = a[1:-1]
+       h = remove.split(", ")
+       h1 = h[0].replace("'", "")
+       f1 = h[1]
+
+       if f1 == "'r'":
+              print "Read"
+              f = open(h1)
+              l = f.read(1024)
+              while (l):
+                     c.send(l)
+                     print('Sent ',repr(l))
+                     l = f.read(1024)
+              f.close()
+              print "File was sent"
+       elif f1 == "'a'":
+              a1 = c.recv(1024)
+              print a1
+              f = open(h1, 'a')
+              #l1 = f.read(1024)
+              l = f.write(a1)
+              print "File Modified"
+              f.close()
+
+       else:
+              print "Failed to Read"
+                           # Close the connection
+
+
+def Main():
+    host = '127.0.0.1'
+    port = 5003
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind(('',port))
+    s.listen(5)
+
+    
+    print('server started')
+    while(True):
+        (c,addr) = s.accept()
+        print('client connected ip:'+str(addr))
+        Thread = client_thread(addr,c)
+        Thread.start()
+
+
+        
+if __name__ == '__main__':    
     Main()
