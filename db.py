0954b0743e8d6ea0ba86c1dd689d47648b00c1de
pooja2012 <33168116+pooja2012@users.noreply.github.com>
2017-12-09 22:51:43 +0000

    Add files via upload

---
+++ db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+print ("Opened database successfully");
+
+print('1')
+cur = conn.cursor()
+cur.execute("SELECT * FROM FILES")
+ 
+rows = cur.fetchall()
+print('2') 
+for row in rows:
+    print('3')
+    print(row)
